2e787078bed16182ecd3ccaae3000c96bd586e9e
BenjaminC4illet <benjamin.caillet@meisterlabs.com>
2022-11-23 16:15:19 +0100

    Tested colors exports on Mobile Platf.

--- svgo.config.js
+++ svgo.config.js
@@ -1,18 +1,23 @@
 module.exports = {
+    js2svg: {
+        indent: 2, // string with spaces or number of spaces. 4 by default
+        pretty: true, // boolean, false by default
+    },
+    multipass: true,
     plugins: [
-      {
-        name: 'preset-default',
-        params: {
-          overrides: {
-            // customize default plugin options
-            inlineStyles: {
-              onlyMatchedOnce: false,
+        {
+            name: 'preset-default',
+            params: {
+                overrides: {
+                    removeViewBox: false,
+                },
             },
-  
-            // or disable plugins
-            removeDoctype: false,
-          },
         },
-      },
+        {
+            name: 'addAttributesToSVGElement',
+            params: {
+                attribute: 'width="100%" height="100%"',
+            },
+        },
     ],
-  };
+};
